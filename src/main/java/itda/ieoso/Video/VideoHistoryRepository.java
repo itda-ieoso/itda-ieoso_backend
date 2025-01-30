7a2b4499363bb007d38da6f5da1528f1b90b4a5d
EunSeo0117 <gmjea023@gmail.com>
2025-01-30 10:36:12 +0900

    feat(history) : 강의실입장시 course의 모든 history생성 기능 추가

--- src/main/java/itda/ieoso/Video/VideoHistoryRepository.java
+++ src/main/java/itda/ieoso/Video/VideoHistoryRepository.java
@@ -1,15 +1,21 @@
 package itda.ieoso.Video;
 
+import itda.ieoso.Course.Course;
+import itda.ieoso.CourseAttendees.CourseAttendees;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 public interface VideoHistoryRepository extends JpaRepository<VideoHistory, Long> {
     @Modifying
     @Query("DELETE FROM VideoHistory m WHERE m.video.videoId = :videoId")
     @Transactional
     void deleteAllByVideoId(@Param("videoId") Long videoId);
+
+    List<VideoHistory> findAllByCourseAndCourseAttendees(Course course, CourseAttendees courseAttendees);
 
 }
--- src/main/java/itda/ieoso/Video/VideoRepository.java
+++ src/main/java/itda/ieoso/Video/VideoRepository.java
@@ -1,6 +1,10 @@
 package itda.ieoso.Video;
 
+import itda.ieoso.Course.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface VideoRepository extends JpaRepository<Video, Long> {
+    List<Video> findAllByCourse(Course course);
 }
