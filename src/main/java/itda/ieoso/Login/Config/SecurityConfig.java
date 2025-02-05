911feadbb3367e6b68224440b4627b72d7fa2708
jiwonniddaaa <jiwonniddaaa02@gmail.com>
2025-02-05 22:58:12 +0900

    feat: token 인증 구현

--- src/main/java/itda/ieoso/Login/Config/SecurityConfig.java
+++ src/main/java/itda/ieoso/Login/Config/SecurityConfig.java
@@ -54,7 +54,7 @@ public class SecurityConfig {
 
         http
                 .authorizeHttpRequests((auth) -> auth
-                        .requestMatchers("/", "/**").permitAll()
+                        .requestMatchers("/login", "/signup", "/auth/**").permitAll()
                         .anyRequest().permitAll());
 
 
