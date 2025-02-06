7048815c25e42f6214125c8c6d0eeff261d46ed4
jiwonniddaaa <jiwonniddaaa02@gmail.com>
2025-02-07 03:30:15 +0900

    fix: 응답 형태 수정

--- src/main/java/itda/ieoso/Material/MaterialController.java
+++ src/main/java/itda/ieoso/Material/MaterialController.java
@@ -1,5 +1,6 @@
 package itda.ieoso.Material;
 
+import itda.ieoso.Response.Response;
 import itda.ieoso.Video.VideoDto;
 import itda.ieoso.Video.VideoService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,27 +13,27 @@ public class MaterialController {
     private MaterialService materialService;
 
     @PostMapping("/{courseId}/{lectureId}/{userId}")
-    public MaterialDto.Response createMaterial(@PathVariable Long courseId,
-                                         @PathVariable Long lectureId,
-                                         @PathVariable Long userId,
-                                         @RequestBody MaterialDto.createRequest request) {
+    public Response<MaterialDto.Response> createMaterial(@PathVariable Long courseId,
+                                                        @PathVariable Long lectureId,
+                                                        @PathVariable Long userId,
+                                                        @RequestBody MaterialDto.createRequest request) {
 
-        return materialService.createMaterial(courseId,lectureId,userId,request);
+        return Response.success("강의자료 생성", materialService.createMaterial(courseId,lectureId,userId,request));
     }
 
     @PatchMapping("/{courseId}/{materialId}/{userId}")
-    public MaterialDto.Response updateMaterial(@PathVariable Long courseId,
+    public Response<MaterialDto.Response> updateMaterial(@PathVariable Long courseId,
                                          @PathVariable Long materialId,
                                          @PathVariable Long userId,
                                          @RequestBody MaterialDto.updateRequest request) {
-        return materialService.updateMaterial(courseId,materialId,userId,request);
+        return Response.success("강의자료 수정", materialService.updateMaterial(courseId,materialId,userId,request));
     }
 
     @DeleteMapping("/{courseId}/{materialId}/{userId}")
-    public MaterialDto.deleteResponse deleteMaterial(@PathVariable Long courseId,
+    public Response<MaterialDto.deleteResponse> deleteMaterial(@PathVariable Long courseId,
                                                @PathVariable Long materialId,
                                                @PathVariable Long userId) {
-        return materialService.deleteMaterial(courseId,materialId,userId);
+        return Response.success("강의자료 삭제", materialService.deleteMaterial(courseId,materialId,userId));
     }
 
 }
