c97fca0964525b913bd59684a611d5469befe58b
EunSeo0117 <gmjea023@gmail.com>
2025-02-09 06:20:54 +0900

    feat(announcement) : 공지 조회수기능 추가

--- src/main/java/itda/ieoso/Announcement/AnnouncementController.java
+++ src/main/java/itda/ieoso/Announcement/AnnouncementController.java
@@ -16,42 +16,49 @@ public class AnnouncementController {
     @Autowired
     private AnnouncementService announcementService;
 
+    // 강의 공지 생성(개설자만)
     @PostMapping("/{courseId}/{userId}")
-    public Response<AnnouncementResponseDto> createAnnouncement(@PathVariable Long courseId,
+    public Response<AnnouncementDto.Response> createAnnouncement(@PathVariable Long courseId,
                                                                 @PathVariable Long userId,
-                                                                @RequestBody AnnouncementRequestDto responseDto) {
+                                                                @RequestBody AnnouncementDto.Request responseDto) {
         return Response.success("강의공지 생성", announcementService.createAnnouncement(courseId, userId, responseDto));
     }
 
+    // 강의공지 업데이트(개설자만)
+    @PatchMapping("/{courseId}/{userId}/{announcementId}")
+    public Response<AnnouncementDto.Response> updateAnnouncement(@PathVariable Long courseId,
+                                                                @PathVariable Long userId,
+                                                                @PathVariable Long announcementId,
+                                                                @RequestBody AnnouncementDto.Request responseDto) {
+
+        return Response.success("강의공지 수정", announcementService.updateAnnouncement(courseId, userId, announcementId, responseDto));
+    }
+
+    // 강의공지 삭제(개설자만)
+    @DeleteMapping("/{courseId}/{userId}/{announcementId}")
+    public Response<AnnouncementDto.deleteResponse> deleteAnnouncement(@PathVariable Long courseId,
+                                          @PathVariable Long userId,
+                                          @PathVariable Long announcementId) {
+
+        announcementService.deleteAnnouncement(courseId, userId, announcementId);
+
+        return Response.success("강의공지 삭제", null);
+    }
+
+
+    // 강의공지 목록 조회(개설자, 수강생) (userid는 courseattendees검증용)
     @GetMapping("/{courseId}/{userId}")
-    public Response<List<AnnouncementResponseDto>> getAnnouncements(@PathVariable Long courseId, @PathVariable Long userId) {
+    public Response<List<AnnouncementDto.Response>> getAnnouncements(@PathVariable Long courseId, @PathVariable Long userId) {
         return Response.success("강의공지 목록 조회", announcementService.getAnnouncements(courseId, userId));
     }
 
+    // 강의공지 상세 조회(개설자, 수강생) (userid는 courseattendees검증용)
     @GetMapping("/{courseId}/{userId}/{announcementId}")
-    public Response<AnnouncementResponseDto> getAnnouncement(@PathVariable Long courseId,
+    public Response<AnnouncementDto.Response> getAnnouncement(@PathVariable Long courseId,
                                 @PathVariable Long userId,
                                 @PathVariable Long announcementId) {
 
         return Response.success("강의공지 상세 조회", announcementService.getAnnouncement(courseId, userId, announcementId));
     }
 
-    @PatchMapping("/{courseId}/{userId}/{announcementId}")
-    public Response<AnnouncementResponseDto> updateAnnouncement(@PathVariable Long courseId,
-                                                                    @PathVariable Long userId,
-                                                                    @PathVariable Long announcementId,
-                                                                    @RequestBody AnnouncementRequestDto responseDto) {
-
-        return Response.success("강의공지 수정", announcementService.updateAnnouncement(courseId, userId, announcementId, responseDto));
-    }
-
-    @DeleteMapping("/{courseId}/{userId}/{announcementId}")
-    public Response<?> deleteAnnouncement(@PathVariable Long courseId,
-                                            @PathVariable Long userId,
-                                            @PathVariable Long announcementId) {
-
-        announcementService.deleteAnnouncement(courseId, userId, announcementId);
-
-        return Response.success("강의공지 삭제", null);
-    }
 }
