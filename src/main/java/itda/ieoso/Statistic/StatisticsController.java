732ec91f4d05af871796bee95b658af9c8036b47
EunSeo0117 <gmjea023@gmail.com>
2025-03-11 21:10:18 +0900

    feat : 공개여부에 따라 수강생들에게 보여지도록 수정

--- src/main/java/itda/ieoso/Statistic/StatisticsController.java
+++ src/main/java/itda/ieoso/Statistic/StatisticsController.java
@@ -16,14 +16,15 @@ public class StatisticsController {
 
     @Autowired private StatisticsService statisticsService;
 
-    // 과제 제출 통계 조회 API
+    // 과제 제출 통계 조회 API (과제 공개여부에 따라 개설자/수강생 둘다 사용가능)
+    // 강의실 요약 / 상세내용 보기 기능 둘다
     @GetMapping("/courses/{courseId}/assignments")
     public Response<List<AssignmentStatisticsDTO>> getAssignmentStatistics(@PathVariable Long courseId) {
         List<AssignmentStatisticsDTO> statistics = statisticsService.getAssignmentStatistics(courseId);
         return Response.success("통계 요약 조회", statistics); // 성공 시 데이터 반환
     }
 
-    // 과제 제출 결과 조회 API
+    // 과제 제출 결과 조회 API (과제 공개여부에 따라 개설자/수강생 둘다 사용가능)
     @GetMapping("/courses/{courseId}/assignments/submissions")
     public Response<List<AssignmentSubmissionDTO>> getAllAssignmentSubmissions(@PathVariable Long courseId) {
         List<AssignmentSubmissionDTO> submissionDetails = statisticsService.getAllAssignmentSubmissions(courseId);
